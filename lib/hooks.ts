90951fd13954c9d5186fecff71861fa74423ca61
thalibar.rifqi <thalibar.rifqi@behaestex.co.id>
2024-05-19 19:25:45 +0700

    Fix: useEffect missing dependency of sectionName Feat: add sectionName on lib/hooks.ts

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -14,7 +14,7 @@ export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
         if (inView && Date.now() - timeOfLastClick > 1000) {
           setActiveSection(sectionName)
         }
-      }, [inView, setActiveSection, timeOfLastClick])
+      }, [inView, setActiveSection, timeOfLastClick, sectionName])
 
     return {
         ref
