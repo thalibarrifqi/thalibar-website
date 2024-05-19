f14d2b043c9844a89e0e263e071dad7be5fb0940
thalibar.rifqi <thalibar.rifqi@behaestex.co.id>
2024-05-19 20:54:04 +0700

    Modify image project position on mobile

--- components/project.tsx
+++ components/project.tsx
@@ -31,7 +31,7 @@ export default function Project({
       className="group mb-3 sm:mb-8 last:mb-0"
     >
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] h-[23rem] hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full ">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -52,7 +52,7 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute sm:block sm:top-8 top-[12rem] sm:-right-40 w-[28.25rem] rounded-t-lg shadow-2xl sm:scale-100 scale-[85%]
+          className="absolute sm:block sm:top-8 top-[13rem] sm:-right-40 w-[28.25rem] rounded-t-lg shadow-2xl sm:scale-100 scale-[85%]
         transition 
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
