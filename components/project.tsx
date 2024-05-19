e994e8bc383707be1f2040b09a4d97e61a490ed5
thalibar.rifqi <thalibar.rifqi@behaestex.co.id>
2024-05-19 20:42:01 +0700

    Add image of projects on mobile device

--- components/project.tsx
+++ components/project.tsx
@@ -30,13 +30,13 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[55%] flex flex-col h-full ">
-          <h3 className="text-2xl font-semibold sm:w-[30rem]">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 max-w-[40%] sm:max-w-[80%] sm:w-[32rem]">
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] h-[23rem] hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full ">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto max-w-[40%] sm:max-w-[100%] ">
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
@@ -52,12 +52,17 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute sm:block top-14 sm:top-8 -right-40 sm:w-[28.25] w-[26rem] rounded-t-lg shadow-2xl
+          className="absolute sm:block sm:top-8 top-[12rem] sm:-right-40 w-[28.25rem] rounded-t-lg shadow-2xl sm:scale-100 scale-[85%]
         transition 
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
         group-hover:translate-y-3
-        group-hover:-rotate-2"
+        group-hover:-rotate-2
+        sm:group-hover:scale-[1.04]
+        sm:group-hover:-translate-x-3
+        sm:group-hover:translate-y-3
+        sm:group-hover:-rotate-2
+        "
         />
       </section>
     </motion.div>
