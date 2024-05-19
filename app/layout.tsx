5aba8a5182995701d3d830f25e2ccc1c46d004ca
thalibar.rifqi <thalibar.rifqi@behaestex.co.id>
2024-05-19 21:02:52 +0700

    Change toast notification to top-center

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
-          <Toaster position="top-right"/>
+          <Toaster position="top-center"/>
           <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
