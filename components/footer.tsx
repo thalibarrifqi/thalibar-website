943f50181dfd46f6b26d9fae4898a3365743edd4
thalibar.rifqi <thalibar.rifqi@behaestex.co.id>
2024-05-19 22:57:51 +0700

    Edit footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,11 +6,11 @@ export default function Footer() {
       <small className="mb-2 block text-xs">
         &copy; 2024 Thalibar Rifqi. All rights reserved.
       </small>
-      <p className="text-xs">
+      {/* <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
         Framer Motion, React Email & Resend.
-      </p>
+      </p> */}
     </footer>
   );
 }
