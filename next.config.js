02ad87ee5c1c96245f2066f43a3a74684545a342
thalibar.rifqi <thalibar.rifqi@behaestex.co.id>
2025-12-08 08:19:20 +0700

    update next.js to 15.0.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+      images: {
+        qualities: [95], // tambahkan nilai yang kamu pakai
+      },
   };
   
   module.exports = nextConfig;
